0b78671adcc576d7d4f2df0fd2f697649c395dc0
Roy Lines <roy@roylines.co.uk>
2012-05-26 15:40:17 +0100

    get user from header

--- server/server.js
+++ server/server.js
@@ -13,15 +13,21 @@ var app = module.exports = express.createServer();
 
 app.use(express.bodyParser());
 
-// curl -F "upload=@package.json" http://127.0.0.1:3000/upload
+// curl -F "upload=@package.json" -H "user: roy" http://127.0.0.1:3000/upload
 
 app.post('/upload', function(req, res){
   console.log('uploading...');
+  console.log(req.headers);
+  var user = req.headers.user;
+  if(user == undefined) {
+    console.log('missing user');
+    return res.send('no user', 404);
+  }
+
   if(req.files == undefined) {
     console.log('missing belly');
     return res.send('no files', 404);
   } else {
-    var user = 'me@me.com';
     var name = 'uploads/' + user + '_' + req.files.upload.lastModifiedDate.toJSON() + '.jpg';
     console.log('belly received: ' + name);
 
