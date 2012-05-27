483e9b19cd5b51a6b93c66ea4a44d50688e8742e
Mark Chapman <mark.chapman@gmail.com>
2012-05-27 02:14:23 +0100

    Use local mongo db to get facts

--- server/lib/site.js
+++ server/lib/site.js
@@ -3,16 +3,37 @@ var express = require('express');
 var site = {}
 
 site.initialise = function(app) {    
-  app.use(express.static(__dirname + '/../static'));
+    app.use(express.static(__dirname + '/../static'));
 	app.set('view engine', 'jade');
 	app.set('view options', { layout: false });
-  app.get('/', site.home);
-  return app;
+    app.get('/', site.home);
+
+    // Load configurations
+    var config_file = require('yaml-config');
+    exports = module.exports = config = config_file.readConfig('config/config.yaml');
+
+    var mongodb = require('mongodb');
+    var server = new mongodb.Server('127.0.0.1', 27017, {});  // Use local server as dodgy connectivity
+
+    var client = new mongodb.Db('wobblies', server);
+    site.thoughts_for_the_day = [];
+    client.open(function(err) {
+        if (err) throw err;
+        client.collection('facts', function(err, collection) {
+            if (err) throw err;
+            console.log('We can now spout facts');
+            collection.find().toArray(function(err, results) {
+                if (err) throw err;
+                site.thoughts_for_the_day = results;
+            });
+        });
+    });
+    return app;
 };
 
 site.home = function(request, response) {
   response.render('index.jade', {
-      thought_for_the_day : 'For every 1kg of weight you lose, you decrease the risk of diabetes by 9%'
+      thought_for_the_day : site.thoughts_for_the_day[Math.floor(Math.random()*site.thoughts_for_the_day.length)].txt
     });
 };
 
